446a66cf404dd248b7f289fa4da0a95d4d1b3bde
Toluwaa <117469685+Toluwaa@users.noreply.github.com>
2022-11-08 20:37:06 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,30 @@
-// write your code here
+// Femi-Awoyale Toluwalope   J00966835  8th November 2022   11th November 2022
 
+#include <iostream>
+using namespace std;
+ 
+ 
+int main(){
+  int num;
+  cin >> num;
+  bool isPrime = true;
+
+  const int prime [] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+  
+
+  if(num > 1000){
+    cout << "Out of bound";
+  }
+  else{
+    for (const int i : prime)
+      if (i != num)
+        if ((num % i) == 0){
+          if (isPrime)
+            cout << "";
+            cout << i << " ";
+          isPrime = false;
+        }
+    if (isPrime)
+    cout << "Number " << num << " is prime!";
+  }
+}
